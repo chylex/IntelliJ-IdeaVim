68f21ad7c9cd5edabe56674a26523cfe7d908f75
chylex <contact@chylex.com>
2021-10-18 13:36:02 +0200

    Set custom plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -344,8 +344,6 @@ tasks {
     val pluginVersion = version
     // Don't forget to update plugin.xml
     patchPluginXml {
-        sinceBuild.set("233.11799.30")
-
         // Get the latest available change notes from the changelog file
         changeNotes.set(
             provider {
