fe90c24a468fb9f33434a0b5495eb68b87790e48
chylex <contact@chylex.com>
2021-10-18 13:36:02 +0200

    Set custom plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -113,6 +113,8 @@ dependencies {
     testFramework(TestFrameworkType.Platform)
     testFramework(TestFrameworkType.JUnit5)
 
+    compatiblePlugin("com.intellij.classic.ui")
+
     pluginModule(runtimeOnly(project(":modules:ideavim-common")))
     pluginModule(runtimeOnly(project(":modules:ideavim-frontend")))
     pluginModule(runtimeOnly(project(":modules:ideavim-backend")))
@@ -208,6 +210,7 @@ tasks {
   // a custom task (see below)
   runIde {
     systemProperty("octopus.handler", System.getProperty("octopus.handler") ?: true)
+    systemProperty("idea.trust.all.projects", "true")
   }
 
   // Uncomment to run the plugin in a custom IDE, rather than the IDE specified as a compile target in dependencies
@@ -385,11 +388,6 @@ tasks {
       }
     })
   }
-
-  buildPlugin {
-    dependsOn(sourcesJar)
-    from(sourcesJar) { into("lib/src") }
-  }
 }
 
 java {
@@ -485,13 +483,7 @@ intellijPlatform {
     )
 
     ideaVersion {
-      // Let the Gradle plugin set the since-build version. It defaults to the version of the IDE we're building against
-      // specified as two components, `{branch}.{build}` (e.g., "241.15989"). There is no third component specified.
-      // The until-build version defaults to `{branch}.*`, but we want to support _all_ future versions, so we set it
-      // with a null provider (the provider is important).
-      // By letting the Gradle plugin handle this, the Plugin DevKit IntelliJ plugin cannot help us with the "Usage of
-      // IntelliJ API not available in older IDEs" inspection. However, since our since-build is the version we compile
-      // against, we can never get an API that's newer - it would be an unresolved symbol.
+      sinceBuild.set("253")
       untilBuild.set(provider { null })
     }
   }
