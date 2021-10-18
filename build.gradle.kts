7c233f5e1a2b5d80aa3402698c6abce5a0ea4d0e
chylex <contact@chylex.com>
2021-10-18 13:36:02 +0200

    Set custom plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -137,6 +137,7 @@ dependencies {
 
     // AceJump is an optional dependency. We use their SessionManager class to check if it's active
     plugin("AceJump", "3.8.19")
+    plugin("com.intellij.classic.ui", "251.23774.318")
 
     bundledPlugins("org.jetbrains.plugins.terminal")
   }
@@ -239,6 +240,8 @@ tasks {
   }
 
   compileTestKotlin {
+    enabled = false
+    
     kotlinOptions {
       jvmTarget = javaVersion
       apiVersion = "2.0"
@@ -254,6 +257,7 @@ tasks {
   // a custom task (see below)
   runIde {
     systemProperty("octopus.handler", System.getProperty("octopus.handler") ?: true)
+    systemProperty("idea.trust.all.projects", "true")
   }
 
   // Uncomment to run the plugin in a custom IDE, rather than the IDE specified as a compile target in dependencies
