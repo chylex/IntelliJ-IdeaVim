c9802505297b9b77af44aab8bfdda4161e7c0cc2
chylex <contact@chylex.com>
2022-06-18 19:59:56 +0200

    Change matchit plugin to use HTML patterns in unrecognized files

--- src/main/java/com/maddyhome/idea/vim/extension/matchit/Matchit.kt
+++ src/main/java/com/maddyhome/idea/vim/extension/matchit/Matchit.kt
@@ -231,7 +231,7 @@ private object FileTypePatterns {
     } else if (fileTypeName == "CMakeLists.txt" || fileName == "CMakeLists") {
       this.cMakePatterns
     } else {
-      return null
+      this.htmlPatterns
     }
   }
 
