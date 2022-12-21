f7e1c9c837129fdd2de0f93feeaf9306c62f393c
chylex <contact@chylex.com>
2022-12-21 04:34:56 +0100

    [VIM-696] Restore visual mode after undo/redo, and disable incompatible actions

--- src/main/java/com/maddyhome/idea/vim/helper/UndoRedoHelper.kt
+++ src/main/java/com/maddyhome/idea/vim/helper/UndoRedoHelper.kt
@@ -15,6 +15,7 @@ import com.intellij.openapi.command.impl.UndoManagerImpl
 import com.intellij.openapi.command.undo.UndoManager
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
+import com.maddyhome.idea.vim.VimPlugin
 import com.maddyhome.idea.vim.api.ExecutionContext
 import com.maddyhome.idea.vim.api.VimEditor
 import com.maddyhome.idea.vim.api.injector
@@ -23,6 +24,8 @@ import com.maddyhome.idea.vim.group.IjOptions
 import com.maddyhome.idea.vim.listener.SelectionVimListenerSuppressor
 import com.maddyhome.idea.vim.newapi.globalIjOptions
 import com.maddyhome.idea.vim.newapi.ij
+import com.maddyhome.idea.vim.state.mode.SelectionType
+import com.maddyhome.idea.vim.state.mode.inVisualMode
 import com.maddyhome.idea.vim.undo.UndoRedoBase
 
 /**
@@ -50,6 +53,7 @@ internal class UndoRedoHelper : UndoRedoBase() {
 
       if (injector.globalIjOptions().oldundo) {
         SelectionVimListenerSuppressor.lock().use { undoManager.undo(fileEditor) }
+        restoreVisualMode(editor)
       } else {
         performUntilFileChanges(editor, { undoManager.isUndoAvailable(fileEditor) }, { undoManager.undo(fileEditor) })
 
@@ -81,6 +85,7 @@ internal class UndoRedoHelper : UndoRedoBase() {
     if (undoManager.isRedoAvailable(fileEditor)) {
       if (injector.globalIjOptions().oldundo) {
         SelectionVimListenerSuppressor.lock().use { undoManager.redo(fileEditor) }
+        restoreVisualMode(editor)
       } else {
         performUntilFileChanges(editor, { undoManager.isRedoAvailable(fileEditor) }, { undoManager.redo(fileEditor) })
         CommandProcessor.getInstance().runUndoTransparentAction {
@@ -114,5 +119,22 @@ internal class UndoRedoHelper : UndoRedoBase() {
 
   private fun ifFilePathChanged(editor: VimEditor, oldPath: String?): Boolean {
     return editor.getPath() != oldPath
+  }
+
+  private fun restoreVisualMode(editor: VimEditor) {
+    if (!editor.inVisualMode && editor.getSelectionModel().hasSelection()) {
+      val detectedMode = VimPlugin.getVisualMotion().autodetectVisualSubmode(editor)
+      
+      // Visual block selection is restored into multiple carets, so multi-carets that form a block are always
+      // identified as visual block mode, leading to false positives.
+      // Since I use visual block mode much less often than multi-carets, this is a judgment call to never restore
+      // visual block mode.
+      val wantedMode = if (detectedMode == SelectionType.BLOCK_WISE)
+        SelectionType.CHARACTER_WISE
+      else
+        detectedMode
+      
+      VimPlugin.getVisualMotion().enterVisualMode(editor, wantedMode)
+    }
   }
 }
