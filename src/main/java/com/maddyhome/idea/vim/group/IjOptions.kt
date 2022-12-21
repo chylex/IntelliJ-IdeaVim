6afd36373180e164b76e456edfbe84bdb46c8288
chylex <contact@chylex.com>
2022-12-21 04:34:56 +0100

    Fix(VIM-696) Restore visual mode after undo/redo, and disable incompatible actions

--- src/main/java/com/maddyhome/idea/vim/group/IjOptions.kt
+++ src/main/java/com/maddyhome/idea/vim/group/IjOptions.kt
@@ -141,7 +141,7 @@ object IjOptions {
   // Temporary feature flags during development, not really intended for external use
   val closenotebooks: ToggleOption =
     addOption(ToggleOption("closenotebooks", GLOBAL, "closenotebooks", true, isHidden = true))
-  val oldundo: ToggleOption = addOption(ToggleOption("oldundo", GLOBAL, "oldundo", false, isHidden = true))
+  val oldundo: ToggleOption = addOption(ToggleOption("oldundo", GLOBAL, "oldundo", true, isHidden = true))
   val unifyjumps: ToggleOption = addOption(ToggleOption("unifyjumps", GLOBAL, "unifyjumps", true, isHidden = true))
 
   // This needs to be Option<out VimDataType> so that it can work with derived option types, such as NumberOption, which
