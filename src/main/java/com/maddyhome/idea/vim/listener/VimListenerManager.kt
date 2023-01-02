62039a00d3e1144d3f15d46afba6e7fb2f15276c
chylex <contact@chylex.com>
2023-01-02 01:16:14 +0100

    Remove update checker

--- src/main/java/com/maddyhome/idea/vim/listener/VimListenerManager.kt
+++ src/main/java/com/maddyhome/idea/vim/listener/VimListenerManager.kt
@@ -75,7 +75,6 @@ import com.maddyhome.idea.vim.handler.correctorRequester
 import com.maddyhome.idea.vim.handler.keyCheckRequests
 import com.maddyhome.idea.vim.helper.GuicursorChangeListener
 import com.maddyhome.idea.vim.helper.StrictMode
-import com.maddyhome.idea.vim.helper.VimStandalonePluginUpdateChecker
 import com.maddyhome.idea.vim.helper.exitSelectMode
 import com.maddyhome.idea.vim.helper.exitVisualMode
 import com.maddyhome.idea.vim.helper.forceBarCursor
@@ -369,8 +368,6 @@ internal object VimListenerManager {
 
         event.editor.putUserData(openingEditorKey, OpeningEditor(openingEditor, owningEditorWindow, isPreview, canBeReused))
       }
-
-      VimStandalonePluginUpdateChecker.instance.pluginUsed()
     }
 
     override fun editorReleased(event: EditorFactoryEvent) {
