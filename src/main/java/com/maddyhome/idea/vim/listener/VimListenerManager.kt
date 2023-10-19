7a9f03fbf7cda553936279ad4d36265e5cbe87b0
chylex <contact@chylex.com>
2023-10-19 23:56:06 +0200

    Reset insert mode when switching active editor

--- src/main/java/com/maddyhome/idea/vim/listener/VimListenerManager.kt
+++ src/main/java/com/maddyhome/idea/vim/listener/VimListenerManager.kt
@@ -119,7 +119,6 @@ import com.maddyhome.idea.vim.helper.vimDisabled
 import com.maddyhome.idea.vim.helper.vimInitialised
 import com.maddyhome.idea.vim.key.noteCaretMoveInInsertSession
 import com.maddyhome.idea.vim.key.resetAbbreviationSession
-import com.maddyhome.idea.vim.listener.VimListenerManager.VimEditorFactoryListener.editorCreated
 import com.maddyhome.idea.vim.newapi.IjVimEditor
 import com.maddyhome.idea.vim.newapi.IjVimSearchGroup
 import com.maddyhome.idea.vim.newapi.InsertTimeRecorder
@@ -502,6 +501,15 @@ object VimListenerManager {
     override fun selectionChanged(event: FileEditorManagerEvent) {
       // We can't rely on being passed a non-null editor, so check for Code With Me scenarios explicitly
       if (VimPlugin.isNotEnabled() || !ClientId.isCurrentlyUnderLocalId) return
+
+      val newEditor = event.newEditor
+      if (newEditor is TextEditor) {
+        val editor = newEditor.editor
+        if (editor.isInsertMode) {
+          editor.vim.mode = Mode.NORMAL()
+          KeyHandler.getInstance().reset(editor.vim)
+        }
+      }
 
       if (!isVirtualBuffer(event.oldFile) && !isVirtualBuffer(event.newFile)) {
         // Vim order: BufLeave → WinLeave → WinEnter → BufEnter
