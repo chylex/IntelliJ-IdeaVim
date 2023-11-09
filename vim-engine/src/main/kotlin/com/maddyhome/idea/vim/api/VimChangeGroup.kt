338e13734704c6fbb24ce5c1d8e27f41ebf39611
chylex <contact@chylex.com>
2023-11-09 09:17:29 +0100

    Add support for count for visual and line motion surround

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/api/VimChangeGroup.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/api/VimChangeGroup.kt
@@ -145,7 +145,7 @@ public interface VimChangeGroup {
     operatorArguments: OperatorArguments,
   )
 
-  public fun insertText(editor: VimEditor, caret: VimCaret, offset: Int, str: String): VimCaret
+  public fun insertText(editor: VimEditor, caret: VimCaret, offset: Int, str: CharSequence): VimCaret
 
   public fun insertText(editor: VimEditor, caret: VimCaret, str: String): VimCaret
 
