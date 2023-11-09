4a200c23a7e6ce6cf7eb78c505b25a417d6ff03d
chylex <contact@chylex.com>
2023-11-09 09:17:29 +0100

    Add support for count for visual and line motion surround

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/api/VimChangeGroupBase.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/api/VimChangeGroupBase.kt
@@ -201,7 +201,7 @@ public abstract class VimChangeGroupBase : VimChangeGroup {
    * @param caret  The caret to start insertion in
    * @param str    The text to insert
    */
-  override fun insertText(editor: VimEditor, caret: VimCaret, offset: Int, str: String): VimCaret {
+  override fun insertText(editor: VimEditor, caret: VimCaret, offset: Int, str: CharSequence): VimCaret {
     (editor as MutableVimEditor).insertText(offset, str)
     val newCaret = caret.moveToInlayAwareOffset(offset + str.length)
 
