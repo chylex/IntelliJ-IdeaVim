0de4b4fdde224bbfba968a150f61edeb0d57a545
chylex <contact@chylex.com>
2023-12-03 09:31:15 +0100

    Automatically add unambiguous imports after running a macro

--- src/main/java/com/maddyhome/idea/vim/group/MacroGroup.kt
+++ src/main/java/com/maddyhome/idea/vim/group/MacroGroup.kt
@@ -21,6 +21,7 @@ import com.maddyhome.idea.vim.api.injector
 import com.maddyhome.idea.vim.helper.MessageHelper
 import com.maddyhome.idea.vim.macro.VimMacroBase
 import com.maddyhome.idea.vim.newapi.IjVimEditor
+import com.maddyhome.idea.vim.newapi.ij
 
 /**
  * Used to handle playback of macros
@@ -88,6 +89,9 @@ internal class MacroGroup : VimMacroBase() {
           }
         } finally {
           keyStack.removeFirst()
+        }
+        if (!isInternalMacro) {
+          MacroAutoImport.run(editor.ij, context.ij)
         }
       }
 
