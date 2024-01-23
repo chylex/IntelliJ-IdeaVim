84c30d1afc50bbac6c643814d0b4da44d1bc8a6e
chylex <contact@chylex.com>
2024-01-23 13:16:02 +0100

    Update search register when using f/t

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/api/VimSearchGroupBase.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/api/VimSearchGroupBase.kt
@@ -1425,8 +1425,7 @@ abstract class VimSearchGroupBase : VimSearchGroup {
    * @param patternOffset   The pattern offset, e.g. `/{pattern}/{offset}`
    * @param direction       The direction to search
    */
-  @TestOnly
-  fun setLastSearchState(
+  override fun setLastSearchState(
     pattern: String,
     patternOffset: String,
     direction: Direction,
