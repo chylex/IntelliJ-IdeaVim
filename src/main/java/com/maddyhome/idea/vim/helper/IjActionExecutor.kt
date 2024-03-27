1a3b34d457ce27d00ea26968137fbe42a146b667
chylex <contact@chylex.com>
2024-03-27 13:24:17 +0100

    Fix(VIM-3364): Exception with mapped Generate action

--- src/main/java/com/maddyhome/idea/vim/helper/IjActionExecutor.kt
+++ src/main/java/com/maddyhome/idea/vim/helper/IjActionExecutor.kt
@@ -20,6 +20,7 @@ import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.actionSystem.ex.ActionUtil
 import com.intellij.openapi.actionSystem.ex.ActionUtil.performDumbAwareWithCallbacks
 import com.intellij.openapi.actionSystem.impl.ProxyShortcutSet
+import com.intellij.openapi.actionSystem.impl.Utils
 import com.intellij.openapi.application.ex.ApplicationManagerEx
 import com.intellij.openapi.command.CommandProcessor
 import com.intellij.openapi.command.UndoConfirmationPolicy
@@ -94,6 +95,7 @@ internal class IjActionExecutor : VimActionExecutor {
       ActionManager.getInstance(),
       0,
     )
+    Utils.initUpdateSession(event)
     // beforeActionPerformedUpdate should be called to update the action. It fixes some rider-specific problems.
     //   because rider use async update method. See VIM-1819.
     // This method executes inside of lastUpdateAndCheckDumb
