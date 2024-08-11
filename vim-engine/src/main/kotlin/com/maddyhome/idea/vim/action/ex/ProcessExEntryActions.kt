f5b194002cb2686c2f3a1f517304ea0c5b7496c5
chylex <contact@chylex.com>
2024-08-11 17:34:51 +0200

    Stop macro execution after a failed search

--- vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/ex/ProcessExEntryActions.kt
+++ vim-engine/src/main/kotlin/com/maddyhome/idea/vim/action/ex/ProcessExEntryActions.kt
@@ -88,8 +88,7 @@ class ProcessSearchEntryAction(private val parentAction: ProcessExEntryAction) :
 
       else -> throw ExException("Unexpected search label ${argument.label}")
     }
-    // Vim doesn't treat not finding something as an error, although it might report either an error or warning message
-    if (offsetAndMotion == null) return Motion.NoMotion
+    if (offsetAndMotion == null) return Motion.Error
     parentAction.motionType = offsetAndMotion.second
     return offsetAndMotion.first.toMotionOrError()
   }
